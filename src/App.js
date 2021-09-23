736c316b9746c8dfef0f29b1d319000c6cd5bbeb
unknown <sv73107535@idat.edu.pe>
2021-09-22 21:33:05 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+// import emojiList from './data/emojis.json';
+import { emojis } from './utils/constants/emojis';
+const isLoggedIn = true;
 
-function App() {
+const App = ()  => {
+  const greeting = "greeting";
+  const displayEmojiName = event => alert(event.target.id);
   return (
-    <div className="Container">
-      <h1>Hello World</h1>
+    <div className="container">
+      <h1 id={greeting}>Hello World</h1>
+      <p>I am Wrinting JSX</p>
+      {isLoggedIn && (
+        <ul>
+        {emojis.map(emoji => (
+          <li>
+            <button onClick={displayEmojiName}>
+              <span role="img" aria-label={emoji.name} id={emoji.name}>{emoji.value}</span>
+            </button>
+          </li>
+        ))}
+      </ul>
+      )}
     </div>
   );
 }
